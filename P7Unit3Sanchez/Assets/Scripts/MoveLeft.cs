39a4b9a7a6fecd0ca7ffcc889c9e7eaa008853eb
752071-tech <752071@student.dusd.net>
2026-03-24 15:26:41 -0700

    3.2
    I am on 3.2 number 5

---
+++ P7Unit3Sanchez/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayerController playerControllerScrpit;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerControllerScrpit =
+        GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
